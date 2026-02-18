8ff45465291bb0e36cee67f323b2aae4e7cc0f70
dmitrykotelev <dzmitry.kotseleu@innowise.com>
2026-02-18 14:42:45 +0300

    task1.2

--- week1/task1/kalk/Program.cs
+++ week1/task1/kalk/Program.cs
@@ -1,20 +1,58 @@
-﻿namespace kalk
+﻿using System.Diagnostics;
+
+namespace kalk
 {
     internal class Program
     {
         static int num1, num2;
-        static string operation;
+        static string? operation;
+        static List<string> operations = ["+","-","*","/",];
 
-        static void Main()
+        static int GetNum()
+        {
+            int _num;
+            try
+            {
+                _num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Eror. Type a number");
+                _num = GetNum();
+                return _num;
+            }
+            return _num; 
+        }
+        static string GetOperation()
+        {
+            string _operation;
+            _operation = Console.ReadLine();
+            bool stat = false;
+
+            foreach (string op in operations)
+                if (op == _operation)
+                    stat = true;
+
+            if (stat)
+                return _operation;
+            else
+            {
+                Console.WriteLine("Wront operation, type a proper operation");
+                _operation = GetOperation();
+                return _operation;
+            }
+        }
+
+        static void MainCycle()
         {
             int result = 0;
 
             Console.WriteLine("Type first number");
-            num1 = Convert.ToInt32 (Console.ReadLine());
+            num1 = GetNum();
             Console.WriteLine("Type operation");
-            operation = Console.ReadLine();
+            operation = GetOperation();
             Console.WriteLine("Type second number");
-            num2 = Convert.ToInt32 (Console.ReadLine());
+            num2 = GetNum();
 
             switch (operation)
             {
@@ -35,13 +73,43 @@
                     }
                 case "/":
                     {
-                        result = num1 / num2;
-                        break;
+                        try
+                        {
+                            result = num1 / num2;
+                            break;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Error. Dividence by 0 detected");
+                            return;
+                            break;
+                        }
                     }
             }
 
             Console.WriteLine($"Result of {operation} operation is {result}");
-            Console.ReadLine();
+        }
+
+
+        static void Main()
+        {
+            int st = 1;
+            while (true)
+            {
+                Console.WriteLine("Hello! \n Type what you want to do \n 1 - Use calculator \n 2 - Exit");
+                try
+                {
+                    st = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Error Reading, try again");
+                    st = Convert.ToInt32(Console.ReadLine());
+                }
+                if (st == 2)
+                return;
+                MainCycle();
+            }
         }
     }
 }
--- week1/task1/kalk/obj/Debug/net10.0/kalk.AssemblyInfo.cs
+++ week1/task1/kalk/obj/Debug/net10.0/kalk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kalk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf4250683d101f8eee773ad74719e8d70306fac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baebf97f7a966febf491c9e116474ef29bb4e84c")]
 [assembly: System.Reflection.AssemblyProductAttribute("kalk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kalk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
