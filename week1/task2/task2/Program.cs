ed7cab9a1ed55beab0e81230b424e21cd4adfe58
dmitrykotelev <dzmitry.kotseleu@innowise.com>
2026-02-25 14:12:05 +0300

    Update Program.cs

--- week1/task2/task2/Program.cs
+++ week1/task2/task2/Program.cs
@@ -2,16 +2,16 @@
 {
     internal class Program
     {
-        const int T = 3000;
+        const int TimeDelay = 3000;
         static void ProcessData(string dataName)
         {
-            Thread.Sleep(T);
+            Thread.Sleep(TimeDelay);
             Console.WriteLine($"Sync Task done, data - {dataName} in {T/1000} seconds");
             return;
         }
         async static Task ProcessDataAsync(string dataName)
         {
-            await Task.Delay(T);
+            await Task.Delay(TimeDelay);
             Console.WriteLine($"Async Task done, data - {dataName} in {T/1000} seconds");
         }
         async static Task Main(string[] args)
