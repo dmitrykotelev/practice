1b01e2958606989efed2ed970ef9c31017deb2ae
dmitrykotelev <dzmitry.kotseleu@innowise.com>
2026-03-05 03:42:44 +0300

    fix

---
+++ week3/task 41/task4.1/task4ModelBase/Repository/Repository.cs
@@ -0,0 +1,21 @@
+﻿using task4ModelBase.Interfaces;
+using task4ModelBase.Database;
+
+namespace task4ModelBase.Repository
+{
+    // переделать под джненерик, упростить, вынести всю реализацию сюда из конкретных репозиториев
+    abstract public class Repository<T> : IRepository<T> where T : IModel
+    {
+        protected DatabaseCore Database;
+        public Repository(DatabaseCore database)
+        {
+            Database = database;
+        }
+        abstract public bool Add(T model);
+
+        abstract public T GetById(int id);
+        abstract public List<T> GetAll();
+        abstract public bool Delete(int id);
+        abstract public bool Update(T model);
+    }
+}
